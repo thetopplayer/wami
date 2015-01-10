3b60c8f19b98a1d3c2353999fd766b53be551043
Robert Lanter <rob@roblanter.com>
2015-01-10 15:25:46 -0500

    commit

--- MyWami/src/com/MyWami/util/Constants.java
+++ MyWami/src/com/MyWami/util/Constants.java
@@ -4,16 +4,23 @@ package com.MyWami.util;
  * Created by robertlanter on 3/3/14.
  */
 public final class Constants {
-	final static public String IP = "http://192.168.15.5/MyWami/";
-	final static public String ASSETS_IP = "http://192.168.15.5:80/Wami/";
-//	final static public String IP = "http://172.31.99.12/";
-//	final static public String IP = "http://192.168.254.42/MyWami/";
-//	final static public String ASSETS_IP = "http://192.168.254.42:80/Wami/";
+//	final static public String IP = "http://192.168.15.6/MyWami/";
+//	final static public String ASSETS_IP = "http://192.168.15.6:80/Wami/";
+
+//	final static public String IP = "http://192.168.1.10/MyWami/";
+//	final static public String ASSETS_IP = "http://192.168.1.10:80/Wami/";
+
+//	final static public String IP = "http://192.168.1.2/MyWami/";
+//	final static public String ASSETS_IP = "http://192.168.1.2:80/Wami/";
+
+	final static public String IP = "http://192.168.254.42/MyWami/";
+	final static public String ASSETS_IP = "http://192.168.254.42:80/Wami/";
+
 	final static public String EMAIL_IP = "http://www.mywami.com/";
 //	final static public String IP = 	"http://localhost:63342/WamiGradle/" ;
 //	final static public String IP = "http://172.31.99.12/";
 //	final static public String IP = "http://10.0.1.11/";
-//	final static public String IP = "http://192.168.1.2/";
+//	final static public String IP = "http://192.168.1.10/";
 //	final static public String IP = "http://192.168.1.75/";
 //		final static public String IP = "http://127.0.0.1/";
 //	final static public String IP = "http://172.19.131.150/";
