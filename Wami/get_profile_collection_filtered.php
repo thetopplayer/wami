ad6eddedc54dce4beb70c767796d4cd25eafa8b4
Robert Lanter <rob@roblanter.com>
2015-02-15 17:14:02 -0500

    commit

--- Wami/get_profile_collection_filtered.php
+++ Wami/get_profile_collection_filtered.php
@@ -19,7 +19,8 @@ $response = array();
 $sql = "SELECT first_name, last_name, image_url, profile_name, tags, ip.identity_profile_id, assign_to_identity_profile_id, rating, default_profile_ind
         FROM profile_group_assign pga, identity_profile ip
         WHERE  pga.assign_to_identity_profile_id = " .$identityProfileId. " AND pga.profile_group_id = " .$profile_group_id. "
-        AND pga.identity_profile_id = ip.identity_profile_id AND pga.delete_ind = 0 AND active_ind = 1";
+        AND pga.identity_profile_id = ip.identity_profile_id AND pga.delete_ind = 0 AND active_ind = 1
+        ORDER BY profile_name";
 
 // Get profile collection data
 //$sql = "SELECT first_name, last_name, image_url, profile_name, tags, ipc.identity_profile_id, assign_to_identity_profile_id, rating, default_profile_ind
