44f04176fe352af8e64b3da5e8c2a7e7e3fb6aee
Robert Lanter <rob@roblanter.com>
2015-03-03 22:24:29 -0500

    commit

---
+++ MyWamiIOS/MyWami/MyWami/get_default_identity_profile_id.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * get_default_identity_profile_id.php
+ *
+ * Created by Rob Lanter
+ *
+ * Date: 5/15/14
+ * Time: 7:13 PM
+ *
+ *  Gets identity profile id for the default profile.
+ */
+$jsonInput = file_get_contents('php://input');
+$data = json_decode($jsonInput);
+$user_id = $data->param1;
+require_once __DIR__ . '/db_connect.php';
+$db = new DB_CONNECT();
+$con = $db->connect();
+
+$sql = "SELECT identity_profile_id
+        FROM identity_profile WHERE user_id = " .$user_id.  " AND default_profile_ind = 1";
+
+$result = mysqli_query($con, $sql) or die(mysqli_error($con));
+
+$response["default_identity_profile_id"] =  array();
+if (mysqli_num_rows($result) > 0) {
+    $row = mysqli_fetch_row($result);
+    $item["identity_profile_id"] = $row[0];
+
+    array_push($response["default_identity_profile_id"], $item);
+    $response["ret_code"] = 0;
+    echo json_encode($response);    ;
+}
+else {
+    $response["ret_code"] = 1;
+    $response["message"] = "No Default Identity Profile Id found";
+    echo json_encode($response);
+}
+
+?>
