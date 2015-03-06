897cf8f12cde05e04178850235f1f72f114ecc2f
Robert Lanter <rob@roblanter.com>
2015-03-06 18:01:37 -0500

    commit

--- MyWamiIOS/MyWami/MyWami/ProfileCollectionController.swift
+++ MyWamiIOS/MyWami/MyWami/ProfileCollectionController.swift
@@ -13,7 +13,22 @@ class ProfileCollectionController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        var nav = self.navigationController?.navigationBar
+        nav?.barStyle = UIBarStyle.Black
 
+        let titleBar = UIImage(named: "actionbar_wami_list.png")
+        let imageView2 = UIImageView(image:titleBar)
+        self.navigationItem.titleView = imageView2
+
+        var image : UIImage = UIImage(named:"wami1.png")!.imageWithRenderingMode(UIImageRenderingMode.AlwaysOriginal)
+        let backButton = UIBarButtonItem(image: image, style: UIBarButtonItemStyle.Plain, target: self, action: "back:")
+        navigationItem.leftBarButtonItem = backButton
+//        self.navigationItem.leftBarButtonItem!.tintColor = UIColor.grayColor()
+//        navigationItem.leftBarButtonItem?.setTitleTextAttributes([NSFontAttributeName: UIFont(name: "Helvetica", size: 24)!], forState: UIControlState.Normal)
+    }
+
+    func back(sender: UIBarButtonItem) {
+        self.navigationController?.popViewControllerAnimated(true)
     }
 
 }
