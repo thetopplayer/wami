4ef2801309cce5e94b822549341658f344b37f63
Robert Lanter <rob@roblanter.com>
2015-03-20 21:31:18 -0400

    commit

---
+++ MyWamiIOS/MyWami/MyWami/ProfileListTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+// Created by Robert Lanter on 3/17/15.
+// Copyright (c) 2015 Robert Lanter. All rights reserved.
+//
+
+import UIKit
+
+class ProfileListTableViewCell: UITableViewCell  {
+
+    @IBOutlet var profileName: UILabel!
+    
+    @IBOutlet var profileNameTxt: UITextField!
+    
+    @IBOutlet var contactNameTxt: UITextField!
+}
