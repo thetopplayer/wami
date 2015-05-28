6439ef3b4da2c6c5381e661142e5ae3ab2322be1
Robert Lanter <rob@roblanter.com>
2015-05-27 23:50:11 -0400

    commit

---
+++ MyWamiIOS/MyWami/MyWami/ImageTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  ImageTableViewCell.swift
+//  MyWami
+//
+//  Created by Robert Lanter on 5/27/15.
+//  Copyright (c) 2015 Robert Lanter. All rights reserved.
+//
+
+import UIKit
+
+class ImageTableViewCell: UITableViewCell  {
+    
+    var imageName : UITextField = UITextField()
+    var fileName : UITextField = UITextField()
+    var imageDescription : UITextField = UITextField()
+    var processImageBtn : UIButton = UIButton()
+    var moreInfoBtn : UIButton = UIButton()
+    var galleryImageView : UIImageView = UIImageView()
+    
+    //    override init(style: UITableViewCellStyle, reuseIdentifier: String!) {
+    //        super.init(style: style, reuseIdentifier: reuseIdentifier)
+    //    }
+    //
+    //    required init(coder aDecoder: NSCoder) {
+    //        fatalError("init(coder:) has not been implemented")
+    //    }
+    
+}
