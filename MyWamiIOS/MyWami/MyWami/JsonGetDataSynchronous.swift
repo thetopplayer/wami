3b8db8d0a1de4a316b0b1f1039cd4bc5a9468adc
Robert Lanter <rob@roblanter.com>
2015-07-14 16:32:55 -0400

    commit

--- MyWamiIOS/MyWami/MyWami/JsonGetDataSynchronous.swift
+++ MyWamiIOS/MyWami/MyWami/JsonGetDataSynchronous.swift
@@ -6,9 +6,12 @@
 //  Copyright (c) 2015 Robert Lanter. All rights reserved.
 //
 import Foundation
+import UIKit
 
 public class JsonGetDataSynchronous {
     func jsonGetData (url: String, params : Dictionary<String, String>) -> JSON {
+        var uiView = UIView()
+        
         var request = NSMutableURLRequest(URL: NSURL(string: url)!)
         request.HTTPMethod = "POST"
         
@@ -21,15 +24,13 @@ public class JsonGetDataSynchronous {
         var error: NSError?
         let dataVal = NSURLConnection.sendSynchronousRequest(request, returningResponse: &response, error: &error)
         if let httpResponse = response as? NSHTTPURLResponse {
-            
+            var retCode = httpResponse.statusCode
+            if retCode != 200 {
+                var message = "httpResponse error: " + toString(retCode)
+                uiView.makeToast(message: message, duration: HRToastDefaultDuration, position: HRToastPositionCenter)
+            }
         }
         var jsonResult: NSDictionary = NSJSONSerialization.JSONObjectWithData(dataVal!, options: NSJSONReadingOptions.MutableContainers, error: nil) as! NSDictionary
-
-        
-//        var response: AutoreleasingUnsafeMutablePointer<NSURLResponse? >= nil
-//        var error: NSErrorPointer = nil
-//        var dataVal: NSData =  NSURLConnection.sendSynchronousRequest(request, returningResponse: response, error: nil)!
-//        var jsonResult: NSDictionary = NSJSONSerialization.JSONObjectWithData(dataVal, options: NSJSONReadingOptions.MutableContainers, error: nil) as NSDictionary
         return JSON(jsonResult)
     }
 }
